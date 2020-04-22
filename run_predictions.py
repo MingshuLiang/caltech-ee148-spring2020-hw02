2de880e387b0313f90567caf91d7b3f235c46afc
MingshuLiang <56246889+MingshuLiang@users.noreply.github.com>
2020-04-22 11:24:11 -0700

    Update run_predictions.py

--- run_predictions.py
+++ run_predictions.py
@@ -1,62 +1,88 @@
 import os
 import numpy as np
 import json
-from PIL import Image
+from PIL import Image, ImageDraw
+from datetime import datetime
 
-def compute_convolution(I, T, stride=None):
+
+
+def compute_convolution(I_o, T, stride=1):
     '''
     This function takes an image <I> and a template <T> (both numpy arrays) 
     and returns a heatmap where each grid represents the output produced by 
     convolution at each location. You can add optional parameters (e.g. stride, 
     window_size, padding) to create additional functionality. 
     '''
-    (n_rows,n_cols,n_channels) = np.shape(I)
+    (n_rows,n_cols,n_channels) = np.shape(I_o)
+    (n_rows_T,n_cols_T,n_channels_T) = np.shape(T) # "k" for "kernel"
+    
+    # zero padding
+    nrTh = int((n_rows_T-1)/2) # for n_rows_T_h
+    ncTh = int((n_cols_T-1)/2) # for n_cols_T_h   
+    I = np.pad(I_o, ((nrTh,nrTh), (ncTh,ncTh), (0, 0)), 'constant')
+    
+    heatmap_rgb = np.zeros((n_rows,n_cols,n_channels))
 
-    '''
-    BEGIN YOUR CODE
-    '''
-    heatmap = np.random.random((n_rows, n_cols))
-
-    '''
-    END YOUR CODE
-    '''
+    # normalize T
+    # "ch" for channel
+    T_n = np.zeros((n_rows_T,n_cols_T,n_channels_T))
+    for ch in range(n_channels_T): 
+        norm_T = np.linalg.norm(T[:,:,ch])
+        T_n[:,:,ch] = T[:,:,ch]/norm_T
+    
+    for i in range(n_rows):
+        for j in range(n_cols):
+            for ch in range(n_channels):
+                # normalize cropped image 
+                norm_I = np.linalg.norm(I[i:i+n_rows_T,j:j+n_cols_T,ch])
+                I_cropped_n = I[i:i+n_rows_T,j:j+n_cols_T,ch]/norm_I
+                heatmap_rgb[i][j][ch] = np.sum(T_n[:,:,ch]*I_cropped_n)
+    
+    # Weighted combinatioin of RGB channels
+    heatmap = 0.8*heatmap_rgb[:,:,0]+0.10*heatmap_rgb[:,:,1]+0.10*heatmap_rgb[:,:,2]
 
     return heatmap
 
 
-def predict_boxes(heatmap):
+def predict_boxes(heatmap_o, n_rows_T, n_cols_T):
     '''
     This function takes heatmap and returns the bounding boxes and associated
     confidence scores.
     '''
-
+    
+    n_rows = np.shape(heatmap_o)[0]
+    n_cols = np.shape(heatmap_o)[1]
+    
     output = []
 
     '''
     BEGIN YOUR CODE
     '''
     
-    '''
-    As an example, here's code that generates between 1 and 5 random boxes
-    of fixed size and returns the results in the proper format.
-    '''
-
-    box_height = 8
-    box_width = 6
-
-    num_boxes = np.random.randint(1,5)
-
-    for i in range(num_boxes):
-        (n_rows,n_cols,n_channels) = np.shape(I)
-
-        tl_row = np.random.randint(n_rows - box_height)
-        tl_col = np.random.randint(n_cols - box_width)
-        br_row = tl_row + box_height
-        br_col = tl_col + box_width
-
-        score = np.random.random()
-
+    nrTh = int((n_rows_T-1)/2) # for n_rows_T_half
+    ncTh = int((n_cols_T-1)/2) # for n_cols_T_half
+    
+    heatmap = np.where(heatmap_o > 0.92, heatmap_o ,0)
+    
+    while np.any(heatmap != 0):
+        score = np.amax(heatmap)
+        idx = np.where(heatmap == score)
+        c_row = idx[0].item(0) # c for center
+        c_col = idx[1].item(0) # c for center
+        tl_row = c_row-nrTh
+        tl_col = c_col-ncTh
+        #print(tl_row,tl_col)
+        br_row = tl_row + 7 #n_rows_k
+        br_col = tl_col + 7 #n_cols_k
         output.append([tl_row,tl_col,br_row,br_col, score])
+        
+        top = np.max([c_row-n_rows_T,0])
+        bottom = np.min([c_row+n_rows_T,n_rows])
+        left = np.max([c_col-n_cols_T,0])
+        right = np.min([c_col+n_cols_T,n_cols])
+        
+        heatmap[top:bottom,left:right] = 0
+        
 
     '''
     END YOUR CODE
@@ -84,14 +110,32 @@ def detect_red_light_mf(I):
     '''
     BEGIN YOUR CODE
     '''
-    template_height = 8
-    template_width = 6
+    # set the path to kernel: 
+    T_path = 'data/kernel'
 
-    # You may use multiple stages and combine the results
-    T = np.random.random((template_height, template_width))
+    # get sorted list of kernels: 
+    T_names = sorted(os.listdir(T_path)) 
+
+    # remove any non-JPEG kernels: 
+    T_names = [f for f in T_names if '.jpg' in f] 
+
+    T = np.zeros((21,9,3))
+    # load kernel
+    for i in range(len(T_names)):
+
+        # read image using PIL:
+        T_c = Image.open(os.path.join(T_path,T_names[i]))
+
+        # convert to numpy array:
+        T += np.asarray(T_c)
+
+    T /= len(T_names)
 
     heatmap = compute_convolution(I, T)
-    output = predict_boxes(heatmap)
+    
+    (n_rows_T,n_cols_T,n_channels_T) = np.shape(T) 
+    
+    output = predict_boxes(heatmap,n_rows_T,n_cols_T)
 
     '''
     END YOUR CODE
@@ -103,28 +147,41 @@ def detect_red_light_mf(I):
 
     return output
 
+
 # Note that you are not allowed to use test data for training.
 # set the path to the downloaded data:
-data_path = '../data/RedLights2011_Medium'
+data_path = 'data/RedLights2011_Medium'
 
 # load splits: 
-split_path = '../data/hw02_splits'
+split_path = 'data/hw02_splits'
 file_names_train = np.load(os.path.join(split_path,'file_names_train.npy'))
-file_names_test = np.load(os.path.join(split_Path,'file_names_test.npy'))
+file_names_test = np.load(os.path.join(split_path,'file_names_test.npy'))
 
 # set a path for saving predictions:
-preds_path = '../data/hw02_preds'
-os.makedirs(preds_path, exist_ok=True) # create directory if needed
+preds_path = 'data/hw02_preds'
+#os.makedirs(preds_path, exist_ok=True) # create directory if needed
+
+# save preds (named with date and time)
+now = datetime.now()
+month = now.strftime("%m")
+day = now.strftime("%d")
+time = now.strftime("%H%M")
+date_time = now.strftime("%m%d%H%M")
+
+preds_name_train = 'preds_train_tweaked' + date_time + '.json'
+preds_name_test = 'preds_test_tweaked' + date_time + '.json'
 
 # Set this parameter to True when you're done with algorithm development:
-done_tweaking = False
+done_tweaking = True
 
 '''
 Make predictions on the training set.
 '''
 preds_train = {}
 for i in range(len(file_names_train)):
-
+    
+    print(i)
+    
     # read image using PIL:
     I = Image.open(os.path.join(data_path,file_names_train[i]))
 
@@ -132,9 +189,9 @@ for i in range(len(file_names_train)):
     I = np.asarray(I)
 
     preds_train[file_names_train[i]] = detect_red_light_mf(I)
+    
 
-# save preds (overwrites any previous predictions!)
-with open(os.path.join(preds_path,'preds_train.json'),'w') as f:
+with open(os.path.join(preds_path,preds_name_train),'w') as f:
     json.dump(preds_train,f)
 
 if done_tweaking:
@@ -153,5 +210,10 @@ if done_tweaking:
         preds_test[file_names_test[i]] = detect_red_light_mf(I)
 
     # save preds (overwrites any previous predictions!)
-    with open(os.path.join(preds_path,'preds_test.json'),'w') as f:
+    with open(os.path.join(preds_path,preds_name_test),'w') as f:
         json.dump(preds_test,f)
+
+
+
+
+
